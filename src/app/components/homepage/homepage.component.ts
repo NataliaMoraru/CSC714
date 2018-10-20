3a19fd23f5cc61269b3a269ef286fb549d90169e
SatoAzury <13bony13@gmail.com>
2018-10-20 16:12:29 -0400

    Magazine pics added

--- src/app/components/homepage/homepage.component.ts
+++ src/app/components/homepage/homepage.component.ts
@@ -8,8 +8,20 @@ import { Component, OnInit } from '@angular/core';
 export class HomepageComponent implements OnInit {
 
   constructor() { }
-
+  grades: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  magazines_pic: string[][] = [["assets/1.1.jpg", "assets/1.2.jpg", "assets/1.3.jpg"], ["assets/2.1.jpg", "assets/2.2.jpg"]];
+magazines: string[];
+ifgradeclicked: boolean = false;
   ngOnInit() {
   }
+  ChangeGrade(grade) {
+    this.ifgradeclicked = true;
+    if (grade == 1) {
+      this.magazines = this.magazines_pic[0];
+    }                      
+    else {                 
+      this.magazines = this.magazines_pic[1];
+    }
 
+  }
 }
